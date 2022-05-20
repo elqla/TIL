0b2665558a494f3724557046e32f789aa24848fe
elqla <elqla19@gmail.com>
2022-05-20 22:58:37 +0900

    study

--- problem_solving/05_study/05.20/진수계산.py
+++ problem_solving/05_study/05.20/진수계산.py
@@ -1,28 +1,20 @@
-def b(t, m, p):
+from collections import deque
+def els(n, t, m, p):
     a = list()
-    for i in range(t*m):
-        b = list(bin(i))[2:]
+    jinsu = n
+    target_list = deque([i for i in range(t*m)])
+    while target_list:
+        target = target_list.popleft()
+        b = []
+        while jinsu<=target:
+            b.append(str(target%jinsu))
+            target = target//jinsu
+        b.append(str(target))
+        b = b[::-1]
         a += b
 
-    text = ''
-    idx = 0   # 튜브의 위치
-    aidx = 0  # 전체 위치
-    while t: # 1 1 2 3 4
-        if idx+1==p:  # idx+1 == 첫번째
-            text += a[aidx]
-            t -= 1    # 구해야하는 개수에서 하나 줄여줌
-        idx += 1
-        aidx += 1
-        if idx==m:  # idx가 전체 멤버수와 같아지면, 초기화해줌
-            idx = 0
-    return text
-
-
-def o(t, m, p):
-    a = list()
-    for i in range(t*m):
-        b = list(oct(i))[2:]
-        a += b
+        if len(a)>=t*m:
+            break
     text = ''
     idx = 0
     aidx = 0
@@ -35,26 +27,6 @@ def o(t, m, p):
         if idx==m:
             idx = 0
     return text
-
-
-def d(t, m, p):
-    a = list()
-    for i in range(t*m):
-        b = list(str(i))
-        a += b
-    text = ''
-    idx = 0
-    aidx = 0
-    while t:
-        if idx+1==p:
-            text += a[aidx]
-            t -= 1
-        idx += 1
-        aidx += 1
-        if idx==m:
-            idx = 0
-    return text
-
 
 def h(t, m, p):
     a = list()
@@ -79,20 +51,14 @@ def h(t, m, p):
 
 def solution(n, t, m, p):
     answer = ''
-    if n==2:
-        answer = b(t, m, p)
-
-    elif n==8:
-        answer = o(t, m, p)
-
-    elif n==10:
-        answer = d(t, m, p)
-
-    elif n==16:
+    if n==16:
         answer = h(t, m, p)
+    else:
+        answer = els(n, t, m, p)
 
     return answer
-print(solution(10, 1, 11, 2)) #"0111"
+
+print(solution(2, 4, 2, 1)) #"0111"
 # n진법, 구할숫자개수t, 참가인원m, 튜브순서 p
 
 
